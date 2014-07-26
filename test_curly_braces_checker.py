aef435bf9b6a771ad184b51e9d511b8197aa1783
Brian Spiering <bspiering@gmail.com>
2014-07-26 11:28:12 -0700

    Refactor - better naming conventions

---
+++ test_curly_braces_checker.py
@@ -0,0 +1,25 @@
+import curly_braces_checker
+import unittest
+
+class TestCurlyBraceCheckers(unittest.TestCase):
+    
+    examples_false = (("}", False), ("{", False), ("{}}", False))
+    
+    examples_true = (("", True), ("{}", True), ("{{}{}}", True))
+
+    examples = examples_false + examples_true
+
+    def test_stack_method_to_known_values(self):
+        """Stack method should return the same boolean values as test cases"""
+        for (s, expected_boolean) in self.examples:              
+            actual_boolean = curly_braces_checker.check_curlies_stack(s)                 
+            self.assertEqual(expected_boolean, actual_boolean)   
+
+    def test_counter_method_to_known_values(self):
+        """Counter method should return the same boolean values as test cases"""
+        for (s, expected_boolean) in self.examples:              
+            actual_boolean = curly_braces_checker.check_curlies_counter(s)
+            self.assertEqual(expected_boolean, actual_boolean) 
+
+if __name__ == '__main__':
+    unittest.main()
