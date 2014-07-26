92764f4174d8f0a1850926806df09827f1cbe2c0
Brian Spiering <bspiering@gmail.com>
2014-07-26 11:03:52 -0700

    Fix topos

--- test_check_curly_braces.py
+++ test_check_curly_braces.py
@@ -10,13 +10,13 @@ class TestCurlyBraceCheckers(unittest.TestCase):
     examples = examples_false + examples_true
 
     def test_stack_method_to_known_values(self):
-        """Stack method should return correct bolleans to test cases"""
+        """Stack method should return the same boolean values as test cases"""
         for (s, expected_boolean) in self.examples:              
             actual_boolean = check_curly_braces.check_curlies_stack(s)                 
             self.assertEqual(expected_boolean, actual_boolean)   
 
     def test_counter_method_to_known_values(self):
-        """Counter method should return correct bolleans to test case"""
+        """Counter method should return the same boolean values as test cases"""
         for (s, expected_boolean) in self.examples:              
             actual_boolean = check_curly_braces.check_curlies_counter(s)
             self.assertEqual(expected_boolean, actual_boolean) 
