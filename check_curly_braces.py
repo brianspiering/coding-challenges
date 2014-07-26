9c8e2e131672fd1c37ab59889ac2039a7d1599af
Brian Spiering <bspiering@gmail.com>
2014-07-26 09:46:42 -0700

    Check curly braces with a stack

---
+++ check_curly_braces.py
@@ -0,0 +1,22 @@
+"""Little Parser Problem Challenge: Matching Pairs Validation
+
+Check if an input string has mismatched curly braces.
+
+For example,
+s = "{}" is valid.
+s = "{}}" is invalid.
+"""
+
+def check_curlies_stack(s):
+    "Check if all curly braces are paired."
+    stack = []
+    for l in s:
+        if l == "{":
+            stack.append("{")
+        elif l == "}":
+            try:
+                stack.pop()
+            except IndexError:
+                return False
+    return len(stack) == 0 # If stack is empty, curlies check passes.
+
