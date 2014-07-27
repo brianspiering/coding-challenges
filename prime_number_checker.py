1cb2d649097f81934b7ecf7b3d2378921fa5ec56
Brian Spiering <bspiering@gmail.com>
2014-07-26 18:00:55 -0700

    Write a prime number checker using brute force

--- prime_number_checker.py
+++ prime_number_checker.py
@@ -1,4 +1,5 @@
-"""Write method that checks if a number is prime or not. A prime is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+""" Write a method that checks if a number is prime or not.
+A prime is a natural number greater than 1 that has no positive divisors other than 1 and itself.
 
 For example:
 2 is prime.
@@ -6,4 +7,5 @@ For example:
 """
 
 def is_prime(n):
-    pass
+    "Check if number is prime with list comprhension."
+    return not any([n % _ == 0 for _ in xrange(2,n)])
