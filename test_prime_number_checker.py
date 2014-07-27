fc4639d07b8d7c871ba641799685e2c0e8d11ee4
Brian Spiering <bspiering@gmail.com>
2014-07-26 18:18:39 -0700

    Refactor testing - different data structure and add message

--- test_prime_number_checker.py
+++ test_prime_number_checker.py
@@ -3,18 +3,18 @@ import unittest
 
 class TestPrimeChecker(unittest.TestCase):
     
-    examples_false = ((1, False), (4, False), (33, False))
-    
-    examples_true = ((2, True), (3, True), (5, True), (31, True))
-
-    examples = examples_false + examples_true
+    test_cases_special = [(1, False)]
+    test_cases_false = [(4, False), (33, False)]
+    test_cases_true = [(2, True), (3, True), (5, True), (31, True)]
+    test_cases = test_cases_false + test_cases_true + test_cases_special
 
     def test_method_to_known_values(self):
         """Method should return the same boolean values as test cases"""
-        for (n, expected_boolean) in self.examples:              
+        for (n, expected_boolean) in self.test_cases:              
             actual_boolean = prime_number_checker.is_prime(n)       
-            print expected_boolean, actual_boolean          
-            self.assertEqual(expected_boolean, actual_boolean)   
+            self.assertEqual(expected_boolean, 
+                            actual_boolean,
+                            "Current method failed for the number: {}".format(n))   
 
 if __name__ == '__main__':
     unittest.main()
