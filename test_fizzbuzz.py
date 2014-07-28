f6b1ce8b6684e7e8572ab384ca9af92526439083
Brian Spiering <bspiering@gmail.com>
2014-07-28 09:55:02 -0700

    Refactor to make simplier
    numbers in, numbers out

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -8,9 +8,9 @@ class TestFizzBuzz(unittest.TestCase):
     test_cases_fizzbuzz = [(15,'FizzBuzz'), (30, 'FizzBuzz')]
     test_cases_strings = test_cases_fizz + test_cases_buzz + test_cases_fizzbuzz
 
-    test_case_numbers = [(1,1), (2,2), (98,98)]
+    test_case_numbers = [1, 2, 4, 98]
 
-    def test_method_to_strings(self):
+    def test_method_with_strings(self):
         """Test numbers are replaced with proper strings"""
         for (n, expected_result) in self.test_cases_strings:              
             actual_result = fizzbuzz.fizzbuzz(n)       
@@ -18,10 +18,10 @@ class TestFizzBuzz(unittest.TestCase):
                             actual_result,
                             "Current method failed for the number: {}".format(n))   
     
-    def test_method_to_numbers(self):
-        for (n, expected_result) in self.test_case_numbers:  
+    def test_method_with_numbers(self):
+        for n in self.test_case_numbers:  
             actual_result = fizzbuzz.fizzbuzz(n)
-            self.assertEqual(expected_result, 
+            self.assertEqual(n, 
                             actual_result,
                             "Current method failed for the number: {}".format(n))  
 
