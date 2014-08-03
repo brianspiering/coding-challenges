8dfc6e2176e74d5609eb3e1ac12b8bf42524ad2f
Brian Spiering <bspiering@gmail.com>
2014-08-02 19:47:11 -0700

    Setup a failing test for sleep_in method

---
+++ sleep_in.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" 
+The parameter weekday is True if it is a weekday, and the parameter vacation is True if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in. 
+
+sleep_in(False, False) → True
+sleep_in(True, False) → False
+sleep_in(False, True) → True
+
+source - http://codingbat.com/prob/p173401
+"""
+
+def sleep_in(weekday, vacation):
+      return None
+
+if __name__ == "__main__":
+    weekday = True
+    vacation = True
+    sleep_in(weekday, vacation) 
---
+++ test_sleep_in.py
@@ -0,0 +1,21 @@
+import sleep_in
+import unittest
+
+class TestSleepIn(unittest.TestCase):
+    
+    # [(input_1, input_2), answer]
+    test_cases= [((False, False), False),
+                ((True, False), False),
+                ((False, True), True),
+                ((True, True), True)]
+
+    def test_method_(self):
+        """Test methods returns expected boolean"""
+        for (boolean_pair, expected_result) in self.test_cases:              
+            actual_result = sleep_in.sleep_in(boolean_pair[0], boolean_pair[1])       
+            self.assertEqual(expected_result, 
+                            actual_result,
+                            "Current method failed for the boolean pair: {}".format(boolean_pair))   
+
+if __name__ == '__main__':
+    unittest.main()
