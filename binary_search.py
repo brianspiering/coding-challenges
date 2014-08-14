9414c4bf58a490f268d08fffbf336e4b141b6ebd
Brian Spiering <bspiering@gmail.com>
2014-08-14 11:57:18 -0700

    Passes basic tests

--- binary_search.py
+++ binary_search.py
@@ -1,9 +1,23 @@
 """ Write a program that does a binary search of a sorted array.
 """
+from __future__ import division
 
 def binary_search(needle, sorted_list):
-    "Return true if needle is in sorted_list."
-    pass
+    "Return true if needle is in sorted_list, else return false."
+
+    if sorted_list != sorted(sorted_list): # Make sure list is sorted
+        sorted_list = sorted(sorted_list)
+
+    left, right = 0, len(sorted_list)-1
+    while left < right:
+        midpoint = (left+right)//2
+        if needle == sorted_list[midpoint]:
+            return True
+        elif needle < sorted_list[midpoint]:
+            right = midpoint-1
+        else:
+            left = midpoint+1
+    return False # Needle in sorted array
 
 if __name__ == "__main__":
     sorted_list = xrange(11)
--- test_binary_search.py
+++ test_binary_search.py
@@ -3,11 +3,15 @@ import unittest
 
 class TestBinarySearch(unittest.TestCase):
 
+    test_cases_true = [(3, [1,2,3,4,5])]  # (needle, sorted_array)
+    test_cases_false = [(4, [1,2,3])] # (needle, sorted_array)    
+    test_cases = test_cases_true + test_cases_false
+
     def test_method_to_built_in_method(self):
         """Method should return the same as 'in'""" 
-        test_case_true = (1, [1,2,3])  # (needle, sorted_array)
-        test_case_false = (4, [1,2,3])  # (needle, sorted_array)    
-        self.assertEqual(test_case_true[0] in test_case_true[1], 
-                        binary_search.binary_search(test_case_true[0], test_case_true[1]))   
+        for (needle, sorted_array) in self.test_cases:                              
+            self.assertEqual(needle in sorted_array,
+                             binary_search.binary_search(needle, sorted_array))    
+
 if __name__ == '__main__':
     unittest.main()
